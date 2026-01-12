d6415983d4e6982347db4459ad48f369fe43ffc5
cgutierrez05 <carlagutierrezc05@gmail.com>
2026-01-12 19:12:49 +0000

    guia24 incompleta

---
+++ dashboard/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def base(request):
+    return render(request, 'dashboard/base.html')
+
+def index(request):
+    return render(request, 'dashboard/index.html')
