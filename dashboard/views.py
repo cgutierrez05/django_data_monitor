0755117bdd248b34a7caa33caed1d90bae64f5b1
cgutierrez05 <carlagutierrezc05@gmail.com>
2026-01-14 18:29:34 +0000

    guia 24 sin la api landing

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+from django.conf import settings
 
 def base(request):
     return render(request, 'dashboard/base.html')
 
 def index(request):
-    return render(request, 'dashboard/index.html')
+    response = requests.get(settings.API_URL)
+
+    posts = response.json()
+
+    total_responses = len(posts)    
+    
+    data = {
+        'title': "Landing Page' Dashboard",
+        'total_responses': total_responses,
+    }
+
+    return render(request, 'dashboard/index.html', data)
