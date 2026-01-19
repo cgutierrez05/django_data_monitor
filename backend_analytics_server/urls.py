3c83c3963f88a04fb313baa819289fd22e7ab6c6
Darwin Leonardo Diaz González <dldiaz@espol.edu.ec>
2026-01-19 03:57:26 +0000

    Pequeños Cambios

--- backend_analytics_server/urls.py
+++ backend_analytics_server/urls.py
@@ -19,10 +19,12 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("dashboard/", include("dashboard.urls")),
     path('login/', auth_views.LoginView.as_view(template_name='security/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
+    path('', RedirectView.as_view(url='/dashboard/index', permanent=False)),
 ]
